b06a2bacba0c63d4959dc337478208260c15ace2
Francisco Marco-Serrano <fmarco@kproductivity.com>
2016-09-08 21:35:39 +0100

    cross-validation in gbm

--- main.R
+++ main.R
@@ -4,6 +4,7 @@ require(h2o)
 
 #########################################################################
 
+cat("reading data \n")
 
 people <- read.csv("~/GitHub/predicting-red-hat-business-value/data/people.csv")
 activity.train <- read.csv("~/GitHub/predicting-red-hat-business-value/data/act_train.csv")
@@ -25,29 +26,38 @@ rm(people, activity.train, activity.test)
 
 ####################################################
 
-h2o.init(nthreads=-1, max_mem_size="6G")
+#Generate new features
+
+train$timelag <- as.Date(train$date) - as.Date(train$pdate) #days activity takes place after user starts
+test$timelag <- as.Date(test$date) - as.Date(test$pdate)    #days activity takes place after user starts
+
+train$pmonth <- substr(train$pdate, 6, 7)
+train$month <- substr(train$date, 6, 7)
+train$pyear <- substr(train$pdate, 1, 4)
+train$year <- substr(train$date, 1, 4)
+
+test$pmonth <- substr(test$pdate, 6, 7)
+test$month <- substr(test$date, 6, 7)
+test$pyear <- substr(test$pdate, 1, 4)
+test$year <- substr(test$date, 1, 4)
+
+####################################################
+
+cat("engage h2o cluster \n")
+
+h2o.init(nthreads=2, max_mem_size="6G")
 h2o.removeAll()
 
 data.hex <- as.h2o(train, destination_frame = "data.hex")
 
-#TODO Improve split considering type1 vs other types
-data.split <- h2o.splitFrame(data=data.hex, 
-                             ratios = 0.75, 
-                             seed = 123)
-
-train.split <- data.split[[1]]
-train1 <- train.split[train.split$activity_category == "type 1", ]
-train2 <- train.split[train.split$activity_category != "type 1", ]
-
-test.split <- data.split[[2]]
-test1 <- test.split[test.split$activity_category == "type 1", ]
-test2 <- test.split[test.split$activity_category != "type 1", ]
-
-rm(data.hex, data.split)
+train1 <- data.hex[data.hex$activity_category == "type 1", ]
+train2 <- data.hex[data.hex$activity_category != "type 1", ]
 
 ####################################################
 
 # GBM
+
+cat("running gbm \n")
 
 y <- "outcome"
 
@@ -55,31 +65,29 @@ y <- "outcome"
 disregard1 <- c(y, "people_id", "pdate", "date", "activity_category", "char10")
 x1 <- setdiff(names(train1), disregard1)
 
-fit.gbm1 <- h2o.gbm(y = y, x = x1,
-                    distribution="bernoulli",
-                    training_frame = train1,
-                    validation_frame = test1,
-                    ntrees=100, max_depth=4, learn_rate=0.1)
+fit.gbm1 <- h2o.gbm(y = y, x = x1, distribution = "bernoulli",
+                    training_frame = train1, nfolds = 10,
+                    ntrees = 100, max_depth = 4, learn_rate = 0.1)
 
 disregard2 <- c("char_1", "char_2", "char_3", "char_4", "char_5", "char_6", "char_7", "char_8", "char_9")
 x2 <- setdiff(x1, disregard2)
 
-fit.gbm2 <- h2o.gbm(y = y, x = x1,
-                    distribution="bernoulli",
-                    training_frame = train2,
-                    validation_frame = test2,
-                    ntrees=100, max_depth=4, learn_rate=0.1)
+fit.gbm2 <- h2o.gbm(y = y, x = x2, distribution = "bernoulli",
+                    training_frame = train2, nfolds = 10,
+                    ntrees = 100, max_depth = 4, learn_rate = 0.1)
+
 
 # Predict
-val.hex <- as.h2o(test, destination_frame = "val.hex")
-val1 <- val.hex[val.hex$activity_category == "type 1", ]
-val2 <- val.hex[val.hex$activity_category != "type 1", ]
+test.hex <- as.h2o(test, destination_frame = "test.hex")
+test1 <- test.hex[test.hex$activity_category == "type 1", ]
+test2 <- test.hex[test.hex$activity_category != "type 1", ]
 
-predict1 <- h2o.predict(fit.gbm1, val1)[,1]
-predict1 <- cbind(as.data.frame(val1$activity_id), as.data.frame(predict1))
-predict2 <- h2o.predict(fit.gbm2, val2)[,1]
-predict2 <- cbind(as.data.frame(val2$activity_id), as.data.frame(predict2))
+predict1 <- h2o.predict(fit.gbm1, test1)[,1]
+predict1 <- cbind(as.data.frame(test1$activity_id), as.data.frame(predict1))
+predict2 <- h2o.predict(fit.gbm2, test2)[,1]
+predict2 <- cbind(as.data.frame(test2$activity_id), as.data.frame(predict2))
 
 prediction <- rbind(predict1, predict2)
+names(prediction) <- c("activity_id", "outcome")
 
-write.csv(prediction, "submission.gbm1.csv", row.names = FALSE)
+write.csv(prediction, "submission.gbm2.csv", row.names = FALSE, quote = FALSE)
